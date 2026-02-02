68c1a572c46c35bbd41d74eef92f33fa40fca86b
matt-letts <matthew.letts@bristol.ac.uk>
2026-02-02 14:59:03 +0000

    added documentation

--- R/functions.r
+++ R/functions.r
@@ -1,15 +1,36 @@
+#' Write table
+#' @param x data frame to write
+#' @param filename output filename
+#' @return none
+#' @export
 my.write.table <- function(x, filename) {
-  cat("saving", basename(filename), "...\n")
-  write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
+    cat("saving", basename(filename), "...\n")
+    write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
 }
 
+#' Extract participant ID from TCGA website
+#' @param id TCGA barcode
+#' @return participant ID
+#' @export
 extract.participant <- function(id) {
-  sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
+    sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
 }
 
+#' Extract partipant tissue
+#' @param id TCGA barcode
+#' @return tissue code
+#' @export
 extract.tissue <- function(id) {
-  sub("TCGA-[^-]+-[^-]+-([0-9]+)[^-]+-.*", "\\1", id)
+    sub("TCGA-[^-]+-[^-]+-([0-9]+)[^-]+-.*", "\\1", id)
 }
+
+#' Extract a specific file from a tar archive
+#' @param tar.file path to tar file
+#' @param extract.file file to extract from tar archive
+#' @param new.file output file name
+#' @param resultsdir directory to extract files to
+#' @return none
+#' @export
 extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
     # get file path to extracted file
     x.file <-
